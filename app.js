aa81164c475dfd38d9e95e25f03f591532244fec
Poyyy414 <jpcambiado@gbox.edu.ph>
2026-01-23 16:56:21 +0800

    1st commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+app.use(bodyParser.json());
+
+const cors = require('cors');
+app.use(cors());
+
+app.get('/', function(req, res) {
+    res.send('Welcome to the ByteTech API!');
+});
+
+// Enpoint for authentication routes
+app.use('/auth', authRoutes);
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+}); 
